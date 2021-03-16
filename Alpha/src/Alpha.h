163f1b070f3540fab918d5c5e9df897cd71351d9
R0bstie <33522448+R0bstie@users.noreply.github.com>
2021-03-16 22:54:36 +0100

    reset

--- Alpha/src/Alpha.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by alpha applications
-
-#include "Alpha/Application.h"
-
-/// ---Entry point--------------
-#include "Alpha/EntryPoint.h" 
-/// ----------------------------
--- README.md
+++
@@ -1,2 +0,0 @@
-# AlphaEngine
- Alpha Engine
